96fd92c70b36e8cd27a55c77b8ab1cf2065607ee
Brian Vaughn <bvaughn@fb.com>
2017-04-26 21:49:49 -0700

    Fixed tweetsUrl redirect route

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 function config () {
 	return {
-		"tweetsUrl": process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://tweets-dev.now.sh",
+		"tweetsUrl": process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://tweets.now.sh",
 		"tweetsServerUrl": process.env.NODE_ENV === "development" ? "http://localhost:5001" : "https://tweets-server.now.sh"
 	}
 }
